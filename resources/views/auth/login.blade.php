65290bc339d1bed7c499a10d3ee9dd5844bf6a4c
Nurfadhilah <nurfadhilahalang480@gmail.com>
2025-05-08 02:53:59 +0800

    ubah login style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,85 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@extends('layouts.guest')
+
+@section('body')
+<body class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        @include('components.application-logo')
+      </div>
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Log in to start your session</p>
+    
+          <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="input-group mb-3">
+              <input type="email" id="email" name="email" class="form-control" placeholder="Email">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="password" id="password" type="password" name="password" class="form-control" placeholder="Password">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-8">
+                <div class="icheck-primary">
+                  <input type="checkbox" id="remember_me" name="remember">
+                  <label for="remember_me">
+                    Remember Me
+                  </label>
+                </div>
+              </div>
+              <!-- /.col -->
+              <div class="col-4">
+                <button type="submit" class="btn btn-primary btn-block">Log In</button>
+              </div>
+              <!-- /.col -->
+            </div>
+          </form>
+    
+          <div class="social-auth-links text-center mb-3">
+            <p>- OR -</p>
+            <a href="#" class="btn btn-block btn-primary">
+              <i class="fab fa-facebook mr-2"></i> Log in using Facebook
+            </a>
+            <a href="#" class="btn btn-block btn-danger">
+              <i class="fab fa-google mr-2"></i> Log in using Google
+            </a>
+          </div>
+          <!-- /.social-auth-links -->
+    
+          <p class="mb-1">
+            @if (Route::has('password.request'))    
+              <a href="{{ route('password.request') }}">I forgot my password</a>
+            @endif
+          </p>
+          <p class="mb-0">
+            <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
+          </p>
+        </div>
+        <!-- /.login-card-body -->
+      </div>
+    </div>
+    <!-- /.login-box -->
+</body>  
+@endsection
+
+
+
+
+
+
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -30,4 +30,50 @@
             </div>
         </div>
     </body>
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>FoundIn | Log in</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('admin-lte') }}/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ asset('admin-lte') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('admin-lte') }}/dist/css/adminlte.min.css">
+
+  <style>
+    body{
+        background: #04695d !important;
+    }
+    .card-body {
+        background: #08a593;
+        color: #ffffff;
+    }
+    div.input-group-text, span {
+        color: #ffffff;
+    }
+    p > a {
+        color: #ffffff;
+    }
+    p > a:hover {
+        color: #dfdfdf;
+    }
+  </style>
+</head>
+
+@yield('body')
+
+<!-- jQuery -->
+<script src="{{ asset('admin-lte') }}/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('admin-lte') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('admin-lte') }}/dist/js/adminlte.min.js"></script>
 </html>
