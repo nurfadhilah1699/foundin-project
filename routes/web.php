b6b756b96420302d9d2ae9a524de6c6e5ca51a3e
Nurfadhilah <nurfadhilahalang480@gmail.com>
2025-05-11 00:09:01 +0800

    perubahan route middleware pada post content, comment dan profile

--- routes/web.php
+++ routes/web.php
@@ -57,18 +57,6 @@ Route::middleware(['auth', 'verified', 'admin'])->group(function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     //Profile
-    Route::get('/profile', function () {
-        // Hanya pengguna yang terverifikasi yang dapat mengakses rute ini
-    });
-
-});
-
-Route::middleware('auth')->group(function () {
-    //Email Verification 
-    Route::get('/email/verify', function () {
-        return view('auth.verify-email');})->name('verification.notice');
-
-    //Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -76,13 +64,20 @@ Route::middleware('auth')->group(function () {
     //Post Content
     Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+    //Post Comment
+    Route::post('/posts/{post}/comments', [PostController::class, 'storeComment'])->name('posts.comments.store');
+});
+
+Route::middleware('auth')->group(function () {
+    //Email Verification 
+    Route::get('/email/verify', function () {
+        return view('auth.verify-email');})->name('verification.notice');
 });
 
 //=== User Post, Comment and Explore ===//
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 Route::get('/explore', [PostController::class, 'explore'])->name('explore');
-
-Route::post('/posts/{post}/comments', [PostController::class, 'storeComment'])->name('posts.comments.store');
 
 require __DIR__.'/auth.php';
